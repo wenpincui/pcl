7ab7c480fd879f5603dbcc25f4c9e90ad09c827b
wenpincui <wenpincui224@gmail.com>
2014-04-29 14:24:48 +0800

    add select.

--- database/db.lisp
+++ database/db.lisp
@@ -81,3 +81,111 @@
      (:id3-size number))))
 
 (defparameter *mp3s* (make-instance 'table :schema *mp3-schema*))
+
+(defun insert-row (names-and-values table)
+  (vector-push-extend (normalized-row names-and-values (schema table))
+                      (rows table)))
+
+(defun normalize-row (names-and-values schema)
+  (loop
+       for column in schema
+       for name = (name column)
+       for value = (or (getf names-and-values name)
+                       (default-value column))
+       collect name
+       collect (normalize-for-column value column)))
+
+(defun normalize-for-column (value column)
+  (funcall (value-normalizer column) value column))
+
+(defun file->row (file)
+  (let ((id3 (read-id3 file)))
+    (list
+     :file (namestring (truename file))
+     :genre (translated-genre id3)
+     :artist (artist id3)
+     :album (album id3)
+     :song (song id3)
+     :track (parse-track (track id3))
+     :year (parse-year (year id3))
+     :id3-size (size id3))))
+
+(defun parse-track (track)
+  (when track
+    (parse-integer track :end (position #\/ track))))
+
+(defun parse-year (year)
+  (when year
+    (parse-integer year)))
+
+(defun load-database (dir db)
+  (let ((count 0))
+    (walk-directory
+     dir
+     #'(lambda (file)
+         (princ #\.)
+         (incf count)
+         (insert-row (file->row file) db))
+     :test #'mp3-p)
+    (format t "~&Loaded ~d files into database." count)))
+
+(defun select (&key (columns t) from where distinct order-by)
+  (let ((rows (rows from))
+        (schema (schema from)))
+    (when where
+      (setf rows (restrict-rows rows where)))
+    (unless (eql columns 't)
+      (setf schema (extract-schema (mklist columns) schema))
+      (setf rows (project-columns rows schema)))
+    (when distinct
+      (setf rows (distinct-rows rows schema)))
+    (when order-by
+      (setf rows (sorted-rows schema (mklist order-by))))
+    (make-instance 'table :rows rows :schema schema)))
+
+(defun mklist (thing)
+  (if (listp thing) thing (list thing)))
+
+(defun extract-schema (column-names schema)
+  (loop for c in column-names collect (find-column c schema)))
+
+(defun find-column (column-name schema)
+  (or (find column-name schema :key #'name)
+      (error "No column:~a in schema:~a" column-name schema)))
+
+(defun restrict-rows (rows where)
+  (remove-if-not where rows))
+
+(defun project-columns (rows schema)
+  (map 'vector (extractor schema) rows))
+
+(defun distinct-rows (rows schema)
+  (remove-duplicates rows :test (row-equality-tester schema)))
+
+(defun sorted-rows (rows schema order-by)
+  (sort (copy-seq rows) (row-comparator order-by schema)))
+
+(defun extractor (schema)
+  (let ((names (mapcar #'name schema)))
+    #'(lambda (row)
+        (loop for c in names collect c collect (getf row c)))))
+
+(defun row-equality-tester (schema)
+  (let ((names (mapcar #'name schema))
+        (tests (mapcar #'equality-predicate schema)))
+    #'(lambda (a b)
+        (loop for name in names and test in tests
+             always (funcall test (getf a name) (getf b name))))))
+
+(defun row-comparator (column-names schema)
+  (let ((comparators (mapcar #'comparator
+                             (extract-schema column-names schema))))
+    #'(lambda (a b)
+        (loop
+             for name in column-names
+             for comparator in comparators
+             for a-value = (getf a name)
+             for b-value = (getf b name)
+             when (funcall comparator a-value b-value) return t
+             when (funcall comparator b-value a-value) return nil
+             finally (return nil)))))
